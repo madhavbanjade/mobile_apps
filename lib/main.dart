c8fe93ae2f7300e2b06a9fca84a82cc2d93c098c
madhavbanjade <madhavbnzd57@gmail.com>
2026-08-11 23:51:27 +0545

    changes

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+
+
+//this is not how we work on flutter we use our class concept from dart and call them in our main function.
+// void main(){
+//   runApp(Text(
+//     'Hy it is my first flutter project.',
+//     textDirection: TextDirection.ltr,
+//   ));
+// }
+
+void main(){
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+@override
+  Widget build(BuildContext context){
+    return Text(
+      'Hello flutter world !',
+     textDirection: TextDirection.rtl, //important
+    );
+  }
+
+}
