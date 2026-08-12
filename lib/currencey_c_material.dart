ea094e19e4d7a0569802511448f9a52160b25f66
madhavbanjade <madhavbnzd57@gmail.com>
2026-08-12 23:08:22 +0545

    changes

---
+++ lib/currencey_c_material.dart
@@ -0,0 +1,74 @@
+
+
+import 'package:flutter/material.dart';
+
+class CurrenceyCMaterialApp extends StatelessWidget {
+const CurrenceyCMaterialApp({super.key});
+
+@override
+Widget build(BuildContext context){
+  final border = OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          style: BorderStyle.solid,
+
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      );
+  return Scaffold(
+    backgroundColor: Colors.cyan,
+    body: Center(
+   child: Column(
+     mainAxisAlignment: MainAxisAlignment.center,
+    //  crossAxisAlignment: CrossAxisAlignment.center, // can not do it because coloumn takes only the text width 
+      children: [
+       const  Text(
+          '0',
+         style: TextStyle(
+          color: Color.fromRGBO(245, 246, 247, 0.988),
+          fontSize: 45,
+          fontWeight: FontWeight.bold
+ 
+        )
+        ),
+
+        Container(
+          //you can add padding and margin and a lots of widgets from container.
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            style: TextStyle(
+              color: Colors.black
+            ),
+           
+          decoration: InputDecoration(
+            hintText: "Enter amount in NPR to convert in USD",
+            hintStyle: TextStyle(
+              color: Colors.black
+            ),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+                 
+                
+          ),
+          keyboardType: TextInputType.numberWithOptions(
+            decimal: true,
+            signed: true
+          ),
+          
+          ),
+        ),
+      
+      ],
+    )
+
+    )
+ 
+  );
+}
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 
+import 'package:currency_converter/currencey_c_material.dart';
 import 'package:flutter/material.dart';
-
-
-
 //this is not how we work on flutter we use our class concept from dart and call them in our main function.
 // void main(){
 //   runApp(Text(
@@ -12,16 +10,37 @@ import 'package:flutter/material.dart';
 // }
 
 void main(){
-runApp(MyApp());
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
 @override
   Widget build(BuildContext context){
-    return Text(
-      'Hello flutter world welcome!',
-     textDirection: TextDirection.ltr, //important
+
+    // return const Text(
+    //   'Hello flutter world welcome!',
+    //  textDirection: TextDirection.ltr, //important
+    // );
+
+    return const MaterialApp(
+      // without scaffold --- problem-1
+      // home: Text("Hello World !!!")
+      //with scaffold 
+      // home: Scaffold(
+      //   //this is not aligning where i want and i can not use text-aign as well --- problem-2
+      //   // body: Text("Hello Flutter World"),
+      //   //for aligning ---- solution and the solution is widgettree.
+      //   body: Center(
+      //     child: Text("Hello Flutter World!"),
+      //   ),
+      // ),
+
+
+      // ----- now we build everything in pieces and collecting it here
+      home: CurrenceyCMaterialApp(),
+      
     );
   }
 
